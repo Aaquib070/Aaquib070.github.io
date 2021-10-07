6e9eede15d58fc507e72494a1f6fd1170b98a384
Aaquib070 <aaquibfaridi003@gmail.com>
2021-10-07 06:14:27 +0000

    new commit from pipeline : 07102021.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb64d2efbb8d3ecc17b51427252cd2a2.js"
+  "/precache-manifest.301174a03076d47c9d50fe0dcff295b4.js"
 );
 
 self.addEventListener('message', (event) => {
